e9aed39ad6d6f2b96227aa2d8935f137dcc5c416
dnil2202 <dnilsibarani@gmail.com>
2022-09-28 09:20:48 +0700

    APKG2-4

--- projects/client/src/App.js
+++ projects/client/src/App.js
@@ -1,8 +1,11 @@
+import './App.css'
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from '../src/components/NavbarComponent'
 import Footer from '../src/components/FooterComponent'
 import LandingPages from '../src/pages/LandingPages.jsx'
+import Register from './pages/Register';
+import 'react-phone-number-input/style.css'
 
 
 
@@ -11,9 +14,10 @@ function App() {
   return (
     <div>
       <Navbar/>
-      <Routes>
-        <Route path='/' element={<LandingPages/>}/>
-      </Routes>
+        <Routes>
+          <Route path='/' element={<LandingPages/>}/>
+          <Route path='/register' element={<Register/>}/>
+        </Routes>
       <Footer/>
 
     </div>
---
+++ projects/server/src/config/encript.js
@@ -0,0 +1,28 @@
+const Crypto = require('crypto')
+const jwt = require('jsonwebtoken')
+
+module.exports={
+    hashPassword:(pass)=>{
+        return Crypto.createHmac('sha256','MEDCARE123').update(pass).digest('hex');
+    },
+
+    createToken :(payload,expiresIn='24h')=>{
+        return jwt.sign(payload, 'medcare',{
+            expiresIn
+        })
+    },
+
+    readToken:(req,res,next)=>{
+        console.log('data token', req.token)
+        jwt.verify(req.token,'medcare',(err,decode)=>{
+            if(err){
+                return res.statu(401).send({
+                    message:'Authenticate error'
+                })
+            }
+            console.log('Translate token',decode);
+            req.dataToken = decode
+            next()  
+        })
+    }
+}
