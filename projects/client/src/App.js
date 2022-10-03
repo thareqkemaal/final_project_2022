9e17a08ced8801749624928a0b1f74ebd54a626b
Abdi-01 <abdialghi@gmail.com>
2022-10-03 11:44:23 +0700

    develop-abdi : fix multiple dashboard component

--- projects/client/src/App.js
+++ projects/client/src/App.js
@@ -93,7 +93,7 @@ function App() {
         <Route path='/product' element={<ProductPage />} />
         <Route path='/cart' element={<UserCart />} />
         <Route path='/checkout' element={<Checkout />} />
-        <Route path='/dashboard' element={<Dashboard />} />
+        {/* <Route path='/dashboard' element={<Dashboard />} /> */}
       </Routes>
       <Footer />
     </div>
