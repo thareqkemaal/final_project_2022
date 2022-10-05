e4555ca02a56966759e9c1492d20c3b348dbcbfd
thareqkemaal_github <thareq.kemal24@gmail.com_github>
2022-10-05 09:50:11 +0700

    fix in landing page, double useNavigate and adding <div></div> ini return landing page

--- projects/client/src/pages/LandingPages.jsx
+++ projects/client/src/pages/LandingPages.jsx
@@ -21,8 +21,6 @@ import 'react-toastify/dist/ReactToastify.css';
 const LandingPages = () => {
   const navigate = useNavigate()
 
-  const navigate = useNavigate();
-
   const { email, status, iduser } = useSelector((state) => {
     return {
       email: state.userReducer.email,
@@ -45,98 +43,99 @@ const LandingPages = () => {
   }
   return (
     <div>
-      <div className='px-3 lg:px-[9.5rem]'>
-        {
-          status === 'Unverified' &&
-          <button className='text-red-500 font-Public' onClick={resendVerif}>Click to verified your account</button>
-        }
-        <Carousel />
-        <div className='grid grid-cols-2 my-3 w-full  py-10 shadow-lg bg-gradient-to-tr from-blue-500 to-white rounded-lg'>
-          <div className=''>
-            <img className='h-24 lg:h-52 lg:mx-auto' src={image} alt='medcare.com' />
-          </div>
-          <div className='mx-auto py-2 lg:mx-1 lg:py-4  md:flex justify-around md:mt-10 lg:mt-10'>
+      <div>
+        <div className='px-3 lg:px-[9.5rem]'>
+          {
+            status === 'Unverified' &&
+            <button className='text-red-500 font-Public' onClick={resendVerif}>Click to verified your account</button>
+          }
+          <Carousel />
+          <div className='grid grid-cols-2 my-3 w-full  py-10 shadow-lg bg-gradient-to-tr from-blue-500 to-white rounded-lg'>
             <div className=''>
-              <p className=' text-blue-800 text-sm font-bold lg:text-2xl font-Public'>Punya Resep Dokter</p>
-              <p className='text-white text-xs font-Public'>Hanya foto resep anda dan unggah max 10mb</p>
-              <p className='hidden lg:block text-white text-xs font-Public'>tanpa perlu antri obat akan dikirimkan ke lokasi anda</p>
+              <img className='h-24 lg:h-52 lg:mx-auto' src={image} alt='medcare.com' />
             </div>
-            <div className=' md:pr-2 lg:mt-5'>
-              <button type='button' onClick={() => {
-                if (iduser !== null){
-                navigate('/prescription')
-                } else {
-                  toast.error('You need to login!', {
-                    theme: "colored",
-                    position: "top-center",
-                    autoClose: 2000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: false,
-                    progress: undefined,
-                });
+            <div className='mx-auto py-2 lg:mx-1 lg:py-4  md:flex justify-around md:mt-10 lg:mt-10'>
+              <div className=''>
+                <p className=' text-blue-800 text-sm font-bold lg:text-2xl font-Public'>Punya Resep Dokter</p>
+                <p className='text-white text-xs font-Public'>Hanya foto resep anda dan unggah max 10mb</p>
+                <p className='hidden lg:block text-white text-xs font-Public'>tanpa perlu antri obat akan dikirimkan ke lokasi anda</p>
+              </div>
+              <div className=' md:pr-2 lg:mt-5'>
+                <button type='button' onClick={() => {
+                  if (iduser !== null) {
+                    navigate('/prescription')
+                  } else {
+                    toast.error('You need to login!', {
+                      theme: "colored",
+                      position: "top-center",
+                      autoClose: 2000,
+                      hideProgressBar: false,
+                      closeOnClick: true,
+                      pauseOnHover: true,
+                      draggable: false,
+                      progress: undefined,
+                    });
+                  }
                 }
-              }
-              }
-                className='bg-teal-500 hover:bg-teal-600 px-2 py-1 rounded-lg text-white font-Public'
+                }
+                  className='bg-teal-500 hover:bg-teal-600 px-2 py-1 rounded-lg text-white font-Public'
                 >Unggah Resep</button>
+              </div>
             </div>
           </div>
-        </div>
-        <div className='py-5'>
+          <div className='py-5'>
             <div className='flex justify-between my-2'>
               <p className='text-sm font-bold text-blue-800 font-Public'>Kategori</p>
-              <p className='text-xs font-bold  text-teal-500 font-Public' onClick={()=>navigate('/product')}>Lihat Semua</p>
+              <p className='text-xs font-bold  text-teal-500 font-Public' onClick={() => navigate('/product')}>Lihat Semua</p>
             </div>
-            <div className='flex justify-between mx-auto' onClick={()=>navigate('/product')}>
+            <div className='flex justify-between mx-auto' onClick={() => navigate('/product')}>
               <div className='w-40 rounded-xl shadow-xl '>
                 <div className='py-3'>
-                  <GiMedicines size={50} className='mx-auto fill-red-500'/>
+                  <GiMedicines size={50} className='mx-auto fill-red-500' />
                   <p className='text-xs text-center text-blue-900 font-bold font-Public'>Fever</p>
-                </div>  
+                </div>
               </div>
               <div className='w-40 rounded-xl shadow-xl '>
                 <div className='py-3'>
-                  <GiChestnutLeaf size={50} className='mx-auto fill-green-500'/>
+                  <GiChestnutLeaf size={50} className='mx-auto fill-green-500' />
                   <p className='text-xs text-center text-blue-900 font-bold font-Public'>Flu</p>
-                </div>  
+                </div>
               </div>
               <div className='w-40 rounded-xl shadow-xl '>
                 <div className='py-3'>
-                  <GiShinyApple size={50} className='mx-auto fill-red-500'/>
+                  <GiShinyApple size={50} className='mx-auto fill-red-500' />
                   <p className='text-xs text-center text-blue-900 font-bold font-Public'>Digestive System</p>
-                </div>  
+                </div>
               </div>
               <div className='w-40 rounded-xl shadow-xl '>
                 <div className='py-3'>
-                  <GiSquareBottle size={50} className='mx-auto fill-orange-500'/>
+                  <GiSquareBottle size={50} className='mx-auto fill-orange-500' />
                   <p className='text-xs text-center text-blue-900 font-bold font-Public'>Eyes</p>
-                </div>  
+                </div>
               </div>
               <div className=' hidden md:block w-40 rounded-xl shadow-xl '>
                 <div className='py-3'>
-                  <GiBeerBottle size={50} className='mx-auto fill-gray-500'/>
+                  <GiBeerBottle size={50} className='mx-auto fill-gray-500' />
                   <p className='text-xs text-center text-blue-900 font-bold font-Public'>Vitamin</p>
-                </div>  
+                </div>
               </div>
               <div className='w-40 rounded-xl shadow-xl '>
                 <div className='py-3'>
-                  <GiGlassShot size={50} className='mx-auto fill-red-500'/>
+                  <GiGlassShot size={50} className='mx-auto fill-red-500' />
                   <p className='text-xs text-center text-blue-900 font-bold font-Public' >Asthma</p>
-                </div>  
+                </div>
               </div>
               <div className='hidden md:block w-40 rounded-xl shadow-xl '>
                 <div className='py-3'>
-                  <GiMedicines size={50} className='mx-auto fill-green-500'/>
+                  <GiMedicines size={50} className='mx-auto fill-green-500' />
                   <p className='text-xs text-center text-blue-900 font-bold font-Public'>Hypertension</p>
-                </div>  
+                </div>
               </div>
               <div className='hidden md:block w-40 rounded-xl shadow-xl '>
                 <div className='py-3'>
-                  <GiMedicines size={50} className='mx-auto fill-green-500'/>
+                  <GiMedicines size={50} className='mx-auto fill-green-500' />
                   <p className='text-xs text-center text-blue-900 font-bold font-Public'>Antivirus</p>
-                </div>  
+                </div>
               </div>
             </div>
             <div className='w-40 rounded-xl shadow-xl '>
@@ -153,11 +152,11 @@ const LandingPages = () => {
             </div>
             <div className='flex justify-between my-2'>
               <p className='text-sm font-bold text-blue-800 font-Public'>Produk Populer</p>
-              <p className='text-xs font-bold  text-teal-500 font-Public' onClick={()=>navigate('/product')}>Lihat Semua</p>
+              <p className='text-xs font-bold  text-teal-500 font-Public' onClick={() => navigate('/product')}>Lihat Semua</p>
             </div>
             <div className='bg-gradient-to-t from-teal-50 to-white'>
               <ProductCategory
-              id={1}
+                id={1}
               />
             </div>
             <div className='w-48 shadow-lg mx-4 my-4 bg-white'>
@@ -239,22 +238,24 @@ const LandingPages = () => {
           </div>
         </div>
       </div>
-      <div className='hidden lg:block w-full bg-gray-100'>
-        <div className='p-4'>
-          <p className='text-center text-blue-900 text-sm font-bold font-Public'>Metode Pembayaran</p>
-          <div className='container mx-auto px-20 mt-10'>
-            <div className='flex justify-center'>
-              <img src={bca} className='h-10' alt='medcare.com' />
-              <img src={bri} className='h-10 px-10' alt='medcare.com' />
-              <img src={bni} className='h-10 px-10' alt='medcare.com' />
-              <img src={ovo} className='h-14 px-10' alt='medcare.com' />
-              <img src={gopay} className='h-10 px-10' alt='medcare.com' />
-              <img src={shoppe} className='h-10 px-10' alt='medcare.com' />
+      <div >
+        <div className='hidden lg:block w-full bg-gray-100'>
+          <div className='p-4'>
+            <p className='text-center text-blue-900 text-sm font-bold font-Public'>Metode Pembayaran</p>
+            <div className='container mx-auto px-20 mt-10'>
+              <div className='flex justify-center'>
+                <img src={bca} className='h-10' alt='medcare.com' />
+                <img src={bri} className='h-10 px-10' alt='medcare.com' />
+                <img src={bni} className='h-10 px-10' alt='medcare.com' />
+                <img src={ovo} className='h-14 px-10' alt='medcare.com' />
+                <img src={gopay} className='h-10 px-10' alt='medcare.com' />
+                <img src={shoppe} className='h-10 px-10' alt='medcare.com' />
+              </div>
             </div>
           </div>
         </div>
+        <ToastContainer />
       </div>
-      <ToastContainer/>
     </div>
   )
 }
