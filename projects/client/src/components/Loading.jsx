3b77c29ca1f6c115367c5b8703f9f14958d9a24b
thareqkemaal_github <thareq.kemal24@gmail.com_github>
2022-10-07 10:23:00 +0700

    fix some loading page to another page

--- projects/client/src/components/Loading.jsx
+++ projects/client/src/components/Loading.jsx
@@ -6,7 +6,7 @@ const LoadingComponent = ({ loading }) => {
     if (!loading) return null;
 
     return (
-        <div className="fixed inset-0 bg-transparent backdrop-blur-2xl flex items-center justify-center">
+        <div className="fixed inset-0 bg-transparent backdrop-blur-2xl flex items-center justify-center z-50">
             <div className="p-2 rounded">
                 <img src={logo} className='animate-bounce h-40 bg-white rounded-full' />
             </div>
