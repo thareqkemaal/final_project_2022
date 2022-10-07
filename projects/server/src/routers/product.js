3aaca1ce7f9e6d403564a3f9cf20d5355da539ee
Nur Fatiha Sartika Jimmy <tika@Nurs-MacBook-Air.local>
2022-10-07 12:30:33 +0700

    APKG2-23

--- projects/server/src/routers/product.js
+++ projects/server/src/routers/product.js
@@ -5,6 +5,7 @@ const { productController } = require('../controllers');
 
 route.post('/getproduct', productController.getProduct);
 route.post('/filterproduct/:id', productController.filterProduct);
+route.post('/getproductadmin', productController.getProductAdmin);
 route.get('/getcategory', productController.getCategory);
 route.get('/getcartdata', productController.getcartdata);
 route.delete('/deletecart', productController.deletecart)
